fb1337287aecfbd517fa48d23a0c1ab43560b2ff
Ibai <ik012982i9@206-16-HZ336117.H012982.NET>
2018-05-11 13:24:47 +0200

    Se soluciona el selectporId y se crea el metodo entregao

--- src/modelo/DeliveryModelo.java
+++ src/modelo/DeliveryModelo.java
@@ -7,6 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import objects.Delivery;
+import objects.Subject;
+import objects.Topic;
 
 public class DeliveryModelo extends Conector {
 
@@ -37,7 +39,7 @@ public class DeliveryModelo extends Conector {
 	}
 	//Metodo para seleccionar un delivery por id
 	
-	public ArrayList<Delivery> selectPorId(int id_delivery){
+	public Delivery selectPorId(int id_delivery){
 		//crear arrayList
 				ArrayList<Delivery> deliveries = new ArrayList<Delivery>();
 				
@@ -46,16 +48,14 @@ public class DeliveryModelo extends Conector {
 					pst.setInt(1, id_delivery);
 					//st.setInt(1, "id_delivery");
 					ResultSet rs = pst.executeQuery();
-					while(rs.next()){
+					if (rs.next()) {
 						Delivery delivery = new Delivery();
 						delivery.setId_delivery(rs.getInt("id_delivery"));
 						delivery.setId_assignment(rs.getInt("id_assignment"));
 						delivery.setId_usuario(rs.getInt("id_usuario"));
 						delivery.setFile(rs.getString("file"));
-						
-						deliveries.add(delivery);
+						return delivery;
 					}
-					return deliveries;
 					
 				} catch (SQLException e) {
 					e.printStackTrace();
@@ -63,7 +63,24 @@ public class DeliveryModelo extends Conector {
 				return null;
 	}
 	
-	//update
+	public boolean entregado(int idUser, int idAssignment){
+		
+		PreparedStatement pst;
+		try {
+			pst = super.conexion.prepareStatement("select * from deliveries where id_usuario = ? AND id_assignment = ?");
+		pst.setInt(1, idUser);
+		pst.setInt(2, idAssignment);
+		ResultSet rs = pst.executeQuery();
+		if (rs.next()) {
+			return true;
+		}
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 	
 	public void update(Delivery delivery){
 		//update delivery set id_assignment=4, id_usuario=14, file='unfilerandom' where id_delivery=5
