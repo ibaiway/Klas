cfd7e5ba626f87e3c8521c5a34e49eb50894a4a3
asier <asierfc@gmail.com>
2018-05-22 08:37:20 +0200

    Se crea CrearAsignatura, se configura web.xml y se configuran los datos

---
+++ src/controlador/CrearAsignatura.java
@@ -0,0 +1,14 @@
+package controlador;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CrearAsignatura {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		
+	}
+}
