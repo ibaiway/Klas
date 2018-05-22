7501aace87178467830ac0e2dee2c7b684993c22
Ander <andergonzalez33@gmail.com>
2018-05-22 09:50:19 +0200

    Avances en ListarSubject, me faltan cosas como nose, mostrarlas xD

--- src/controlador/ListarSubject.java
+++ src/controlador/ListarSubject.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,11 +9,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import modelo.SubjectModelo;
+import objects.Subject;
+import objects.User;
+
 public class ListarSubject extends HttpServlet{
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		//Recibe los datos
 		
 		HttpSession session = request.getSession();
+		User userLogged = (User)session.getAttribute("userLogged");
+		SubjectModelo subjectModelo = new SubjectModelo();
+		subjectModelo.selectByUser(userLogged.getId());
+		
+		if(false){
+			
+			
+			ArrayList<Subject> subjects = new ArrayList<Subject>();
+			
+			Subject subject = new Subject();
+			
+		}
 	}
 }
