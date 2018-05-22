3bd282e7480c05532178761cc0d370d261bc82de
Ander <andergonzalez33@gmail.com>
2018-05-22 08:42:05 +0200

    Servlet ListarSubject y esperando a que Eñaut me ayude

--- src/controlador/ListarSubject.java
+++ src/controlador/ListarSubject.java
@@ -8,11 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class ListarTopics extends HttpServlet{
+public class ListarSubject extends HttpServlet{
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-		//primero recibe los datos
-		HttpSession session = request.getSession();
+		//Recibe los datos
 		
+		HttpSession session = request.getSession();
 	}
 }
