9ac5f4141ea5e07fda4235aa254ecdc64147d582
ibaia <ibaia@DESKTOP-P5670OJ>
2018-10-03 00:05:11 +0200

    prueba

--- src/modelo/Config.java
+++ src/modelo/Config.java
@@ -2,9 +2,9 @@ package modelo;
 
 public class Config {
 
-	public static final String HOST = "10.14.2.30";
+	public static final String HOST = "localhost";
 	public static final String BBDD = "klas";
-	public static final String USERNAME = "klas";
-	public static final String PASSWORD = "1234";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "root";
 	
 }
